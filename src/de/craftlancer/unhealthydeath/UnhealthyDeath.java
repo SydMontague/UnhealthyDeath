3239fbf7698776a3faeadbe56d91b53b29d4c8c6
SydMontague <sydmontague@web.de>
2013-02-16 01:00:14 +0100

    Update 0.3 added support for health > 20 changed package to actual existing and owned domain

---
+++ src/de/craftlancer/unhealthydeath/UnhealthyDeath.java
@@ -0,0 +1,32 @@
+package de.craftlancer.unhealthydeath;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UnhealthyDeath extends JavaPlugin
+{
+    public Logger log;
+    UnhealthyListener listener = new UnhealthyListener(this);
+    FileConfiguration config;
+    
+    public void onEnable()
+    {
+        log = getLogger();
+        
+        if (!new File(this.getDataFolder().getPath(), "config.yml").exists())
+            saveDefaultConfig();
+        
+        config = getConfig();
+        
+        getServer().getPluginManager().registerEvents(listener, this);
+    }
+    
+    public void onDisable()
+    {
+        config = null;
+        getServer().getScheduler().cancelTasks(this);
+    }
+}
--- src/de/craftlancer/unhealthydeath/UnhealthyListener.java
+++ src/de/craftlancer/unhealthydeath/UnhealthyListener.java
@@ -1,35 +1,33 @@
-package com.syd.unhealthydeath;
+package de.craftlancer.unhealthydeath;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
-public class ServerPlayerListener implements Listener
+public class UnhealthyListener implements Listener
 {
     public static UnhealthyDeath plugin;
 
-    public ServerPlayerListener(UnhealthyDeath instance)
+    public UnhealthyListener(UnhealthyDeath instance)
     {
         plugin = instance;
     }
 
     @EventHandler
-    public void onPlayerRespawn(PlayerRespawnEvent e)
-    {
-        final Player p = e.getPlayer();
+    public void onPlayerRespawn(PlayerRespawnEvent event)
+    {        
+        final Player p = event.getPlayer();
         final int food;
         final int health = plugin.config.getInt("health.sethealth");
-
+        
         if (plugin.config.getString("food.foodchange").equalsIgnoreCase("set"))
             food = plugin.config.getInt("food.setfood");
         else if (plugin.config.getString("food.foodchange").equalsIgnoreCase("keep"))
             food = p.getFoodLevel() - plugin.config.getInt("food.substractfood");
         else
             food = 0;
-
-        final float b = p.getSaturation();
-        final float c = p.getExhaustion();
+        
         plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable()
         {
             public void run()
@@ -43,12 +41,9 @@ public class ServerPlayerListener implements Listener
                     else
                         p.setFoodLevel(food);
                 }
-
-                p.setSaturation(b);
-                p.setExhaustion(c);
-
-                if (health <= 0 || health > 20)
-                    p.setHealth(20);
+                
+                if (health <= 0 || health > p.getMaxHealth())
+                    p.setHealth(p.getMaxHealth());
                 else
                     p.setHealth(health);
             }
