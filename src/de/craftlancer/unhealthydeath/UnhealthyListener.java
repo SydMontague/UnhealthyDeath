f5beedaa75eed682d5f6cd904931e579c83fc5aa
SydMontague <sydmontague@web.de>
2013-12-01 15:49:49 +0100

    Use a private class instead of a anonymous class.

--- src/de/craftlancer/unhealthydeath/UnhealthyListener.java
+++ src/de/craftlancer/unhealthydeath/UnhealthyListener.java
@@ -32,16 +32,33 @@ public class UnhealthyListener implements Listener
         final float b = p.getSaturation();
         final float c = p.getExhaustion();
         
-        new BukkitRunnable()
+        new DeathTask(p, food, health, b, c).runTaskLater(plugin, 1L);
+    }
+    
+    private class DeathTask extends BukkitRunnable
+    {
+        private final Player p;
+        private final int food;
+        private final double health;
+        private final float saturation;
+        private final float exhaustion;
+        
+        public DeathTask(Player p, int food, double health, float saturation, float exhaustion)
         {
-            @Override
-            public void run()
-            {
-                p.setSaturation(b);
-                p.setExhaustion(c);
-                p.setFoodLevel(food);
-                p.setHealth(health);
-            }
-        }.runTaskLater(plugin, 1L);
+            this.p = p;
+            this.food = food;
+            this.health = health;
+            this.saturation = saturation;
+            this.exhaustion = exhaustion;
+        }
+        
+        @Override
+        public void run()
+        {
+            p.setSaturation(saturation);
+            p.setExhaustion(exhaustion);
+            p.setFoodLevel(food);
+            p.setHealth(health);
+        }
     }
 }
